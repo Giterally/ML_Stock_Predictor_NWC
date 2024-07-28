67324674194df9b747a9a84ed697e490d093b96b
Noah Chander <noahwchander@gmail.com>
2024-07-28 17:14:27 +0100

    Update lstm_comb.py

--- lstm_comb.py
+++ lstm_comb.py
@@ -6,39 +6,36 @@ import torch
 import torch.nn as nn
 
 file_path = r"C:\Users\zcemrpo\OneDrive - University College London\Downloads\CODE\pytorch\AMZN.csv"
-#daily stock history of amazon
+# Daily stock history of amazon
 data = pd.read_csv(file_path)
 
-#simplifying data to relevant columns
+# Simplifying data to relevant columns
 data = data[["Date", "Close"]]
 
-#tells pytorch to use gpu (default use is cpu)
+# Tells pytorch to use gpu (default use is cpu)
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 #print(device)  this should give 'gpu' if using gpu or 'cpu' otherwise
 
-#simple transformations
+# Simple transformations
 #make the date column a pandas datatype
 data["Date"] = pd.to_datetime(data["Date"])
 
 '''
-#plot the data against close price
+# Plot the data against close price
 plt.plot(data["Date"], data["Close"])
 plt.show()
 '''
 
-#now need to set up the dataframe in a way that's close to how
-#the model is going to take the input and train off it.
-#an LSTM is looking at history - for a particular date it wants to know the closing
-#price at the data and the day before that and before that etc:
+# Now need to set up the dataframe in a way that's close to how the model is going to take the input and train off it. An LSTM is looking at history - for a particular date it wants to know the closing price at the data and the day before that and before that etc.
 
 from copy import deepcopy as dc
 
-def prepare_dataframe_for_lstm(df, n_steps): #the number of steps is the 'lookback window'
-    #make a deepcopy of the dataframe
+def prepare_dataframe_for_lstm(df, n_steps): # The number of steps is the 'lookback window'
+    # Make a deepcopy of the dataframe
     df = dc(df)
-    #set the index of the dataframe to 'date'
+    # Set the index of the dataframe to 'date'
     df.set_index("Date", inplace=True)
-    #shifts the dataframe for the number of lookback windows
+    # Shifts the dataframe for the number of lookback windows
     for i in range(1, n_steps+1):
         df[f"Close(t-{i})"] = df["Close"].shift(i)
 
@@ -50,46 +47,32 @@ shifted_df = prepare_dataframe_for_lstm(data, lookback)
 
 #print(shifted_df)
 
-#the dateframe produced has the closing price
-#for each date and then the 7 previous day closing prices
-#This is useful because we now have an input and output pair:
-#an input matrix x and an output vector y. 
-#The input matrix is the previous 7 days of closing prices:
-#"Close(t-1)" -> "Close(t-7)" and the output vector is the
-#current day closing price "Close"
-#the input matrix is used to predict the output vector
-#we are trying to learn the sequential pattern which is why
-#we are using a sequential-based model: rnn (recurrent neural networks)
-#e.g. recurrent network reads going down, going down etc...
-#and therefore predicts the price will go down
+# The dateframe produced has the closing price for each date and then the 7 previous day closing prices. This is useful because we now have an input and output pair: an input matrix x and an output vector y. The input matrix is the previous 7 days of closing prices: "Close(t-1)" -> "Close(t-7)" and the output vector is the current day closing price "Close" the input matrix is used to predict the output vector. We are trying to learn the sequential pattern which is why we are using a sequential-based model: rnn (recurrent neural networks).
 
 shifted_df_as_np = shifted_df.to_numpy()
 #print(shited_df_as_np)
 
-#just going to run a scaler on all of the data
+# Just going to run a scaler on all of the data
 from sklearn.preprocessing import MinMaxScaler
 
-#the feature range puts all the values between -1 and 1
+# The feature range puts all the values between -1 and 1
 scaler = MinMaxScaler(feature_range = (-1, 1))
-#run the transform and fit it onto the matrix
+
+# Run the transform and fit it onto the matrix
 shifted_df_as_np = scaler.fit_transform(shifted_df_as_np)
 #print(shifted_df_as_np)
 
-#now define an x and y
+# Now define an x and y
 X = shifted_df_as_np[:, 1:]
 y = shifted_df_as_np[:, 0]
 
 #print(X.shape, y.shape)
 #prints (6509, 7) and (6059, 0) so same number of rows but X has 7 different features
 
-#mirror X.shape in the horizontal direction so it goes from the
-#Close(t-7) row to the Close(t-1) row since for LSTMs we want it
-#to recurringly get the most updated answer until it gets as
-#close to the real Close value as possible
+# Mirror X.shape in the horizontal direction so it goes from the Close(t-7) row to the Close(t-1) row since for LSTMs we want it to recurringly get the most updated answer until it gets as close to the real Close value as possible.
 X = dc(np.flip(X, axis = 1))
 
-#split into train and test data
-#use the first 95% as train and the last 5% as test
+# Split into train and test data, using the first 95% as train and the last 5% as test.
 split_index = int(len(X) * 0.95)
 
 X_train = X[:split_index]
@@ -101,10 +84,7 @@ y_test = y[split_index:]
 #print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 #prints ((6183, 7), (326, 7), (6183,), (326,))
 
-#requirement for pytorch LSTMs to have an extra dimension at the
-#end so need to do a bit of reshaping where the matricies and y
-#vectors get another dimension at the end.
-
+# Requirement for pytorch LSTMs to have an extra dimension at the end so need to do a bit of reshaping where the matricies and y vectors get another dimension at the end.
 X_train = X_train.reshape((-1, lookback, 1))
 X_test = X_test.reshape((-1, lookback, 1))
 
@@ -114,9 +94,7 @@ y_test = y_test.reshape((-1, 1))
 #print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 #prints ((6183, 7, 1), (326, 7, 1), (6183, 1), (326, 1))
 
-#this is all in numpy, but since we are using pytorch
-#wrap this in pytorch tensors
-
+# This is all in numpy, but since we are using pytorcH. Wrap this in pytorch tensors.
 X_train = torch.tensor(X_train).float()
 y_train = torch.tensor(y_train).float()
 X_test = torch.tensor(X_test).float()
@@ -125,7 +103,7 @@ y_test = torch.tensor(y_test).float()
 #print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 #prints (torch.Size([6183, 7, 1]), torch.Size([6183, 7, 1]), torch.Size([326, 7, 1]), torch.Size([6183, 1]), torch.Size([326, 1]))
 
-#generally when training models in pytorch, you use datasets rather than raw tensors, so we are going to creat the dataset object:
+# Generally when training models in pytorch, you use datasets rather than raw tensors, so we are going to creat the dataset object:
 
 from torch.utils.data import Dataset
 
@@ -144,7 +122,7 @@ class TimeSeriesDataset(Dataset):
 train_dataset = TimeSeriesDataset(X_train, y_train)
 test_dataset = TimeSeriesDataset(X_test, y_test)
 
-#wrap the datasets in dataloaders to get batches
+# Wrap the datasets in dataloaders to get batches
 
 from torch.utils.data import DataLoader
 batch_size = 16
@@ -152,7 +130,7 @@ train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 
-#useful for visualisation
+# Useful for visualisation
 for _, batch in enumerate(train_loader):
     x_batch, y_batch = batch[0].to(device), batch[1].to(device)
     print(x_batch.shape, y_batch.shape)
@@ -162,7 +140,7 @@ for _, batch in enumerate(train_loader):
 
 
 class LSTM(nn.Module):
-    #takes in input_size: number of features 1, hidden_size: however dimension we want to be in the middle, we want 4 num_stacked_layers: you can stack LSTMs because as they recurrently run through themselves, they produce a sequence of hidden layers, we are using 1 since the model is going to overfit pretty well anyway.
+    # Takes in input_size: number of features 1, hidden_size: however dimension we want to be in the middle, we want 4 num_stacked_layers: you can stack LSTMs because as they recurrently run through themselves, they produce a sequence of hidden layers, we are using 1 since the model is going to overfit pretty well anyway.
     def __init__(self, input_size, hidden_size, num_stacked_layers):
         super().__init__()
         self.hidden_size = hidden_size
@@ -170,7 +148,8 @@ class LSTM(nn.Module):
 
         self.lstm = nn.LSTM(input_size, hidden_size, num_stacked_layers, 
                             batch_first=True)
-        #after LSTM, it does the recurrent part, you want a fully connected layer which maaps from the hidden_size to 1, because at the end of the model we need it to predict just one value
+        
+        # After LSTM, it does the recurrent part, you want a fully connected layer which maaps from the hidden_size to 1, because at the end of the model we need it to predict just one value
         self.fc = nn.Linear(hidden_size, 1)
 
     def forward(self, x):
@@ -188,29 +167,33 @@ model
 
 
 def train_one_epoch():
-    #set the model to training mode and print the epoch
+    
+    # Set the model to training mode and print the epoch
     model.train(True)
     print(f'Epoch: {epoch + 1}')
-    #start to accumulate a running loss value
+    
+    # Start to accumulate a running loss value
     running_loss = 0.0
     
     for batch_index, batch in enumerate(train_loader):
         x_batch, y_batch = batch[0].to(device), batch[1].to(device)
         
         output = model(x_batch)
-        #comparing the model's output to the ground truth (real value) is the loss
-        #the loss is a tensor with one value
+        
+        # Comparing the model's output to the ground truth (real value) is the loss - the loss is a tensor with one value.
         loss = loss_function(output, y_batch)
         running_loss += loss.item()
         
-        #zero out the gradient
+        # Zero out the gradient
         optimizer.zero_grad()
-        #do a backward pass through the loss to calculate the gradient
+        
+        # Do a backward pass through the loss to calculate the gradient
         loss.backward()
-        #take a slight step in the direction of the gradient to make the model the model better
+        
+        # Take a slight step in the direction of the gradient to make the model the model better
         optimizer.step()
 
-        #for every 100 batches, get the average loss every 100 batches and print it out
+        # For every 100 batches, get the average loss every 100 batches and print it out
         if batch_index % 100 == 99:
             avg_loss_across_batches = running_loss / 100
             print('Batch {0}, Loss: {1:.3f}'.format(batch_index+1,
@@ -219,7 +202,8 @@ def train_one_epoch():
     print()
 
 def validate_one_epoch():
-    #model train is false, so in valuation mode
+    
+    # Model train is false, so in valuation mode
     model.train(False)
     running_loss = 0.0
     
@@ -228,7 +212,8 @@ def validate_one_epoch():
         
         with torch.no_grad():
             output = model(x_batch)
-            #get and accumulate loss again
+            
+            # Get and accumulate loss again
             loss = loss_function(output, y_batch)
             running_loss += loss.item()
 
@@ -241,10 +226,11 @@ def validate_one_epoch():
 
 
 
-#the training loop 
+# The training loop 
 learning_rate = 0.001
 num_epochs = 10
-#the loss function is the mean squared error since it is a from of regression problem - predicting a continuous value
+
+# The loss function is the mean squared error since it is a from of regression problem - predicting a continuous value.
 loss_function = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
@@ -256,7 +242,7 @@ for epoch in range(num_epochs):
 
 
 
-#code to do some plotting
+# Code to do some plotting
 with torch.no_grad():
     #put all the X_train stuff in to get predictions for the first 95% of the data, remember the model has already seen this stuff so it should do well
     predicted = model(X_train.to(device)).to('cpu').numpy()
@@ -272,19 +258,19 @@ plt.show()
 '''
 
 
-#for the abovve axis, the 'Close' values are between -1 and 1, so we need to do the opposite of what our transform did so that we can get out original scale back - the dollar values which are actually useful
+# For the abovve axis, the 'Close' values are between -1 and 1, so we need to do the opposite of what our transform did so that we can get out original scale back - the dollar values which are actually useful.
 train_predictions = predicted.flatten()
-#it's all about making a dummy matrix with the same shape as what the scalar is used to
-#the dummy matrix is used such that it's first column is our predictions and then apply the inverse transform
+
+# It's all about making a dummy matrix with the same shape as what the scalar is used to. The dummy matrix is used such that it's first column is our predictions and then apply the inverse transform.
 dummies = np.zeros((X_train.shape[0], lookback+1))
 dummies[:, 0] = train_predictions
 dummies = scaler.inverse_transform(dummies)
 
-#get back train predictions in the right scale
+# Get back train predictions in the right scale
 train_predictions = dc(dummies[:, 0])
 #print(train_predictions) gives array([  0.3959165 ,   0.39559413,   0.39486046, ..., 172.31433271, 171.69293455, 171.57329039])
 
-#for the ground truth / y_train, do the same thing
+# For the ground truth / y_train, do the same thing
 dummies = np.zeros((X_train.shape[0], lookback+1))
 dummies[:, 0] = y_train.flatten()
 dummies = scaler.inverse_transform(dummies)
@@ -294,7 +280,7 @@ new_y_train = dc(dummies[:, 0])
 
 
 '''
-#redo plot
+# Redo plot
 plt.plot(new_y_train, label='Actual Close')
 plt.plot(train_predictions, label='Predicted Close')
 plt.xlabel('Day')
@@ -303,7 +289,7 @@ plt.legend()
 plt.show()
 '''
 
-#get the test predictions and convert to the proper scale
+# Get the test predictions and convert to the proper scale
 test_predictions = model(X_test.to(device)).detach().cpu().numpy().flatten()
 
 dummies = np.zeros((X_test.shape[0], lookback+1))
@@ -326,9 +312,7 @@ plt.ylabel('Close')
 plt.legend()
 plt.show()
 
-#Before we pull out the data, we are going to transform the prices and predictions into a binary system.
-#Generally, directionality of price is superior to predicting absolute price and this had been used in the Random Forest Generator Model.
-#In order to compare the 2 models, we can convert both to a binary system.
+#Before we pull out the data, we are going to transform the prices and predictions into a binary system. Generally, directionality of price is superior to predicting absolute price and this had been used in the Random Forest Generator Model. In order to compare the 2 models, we can convert both to a binary system.
 
 # Convert to 1 if the price is higher than the previous day, else 0
 new_y_test_binary = (new_y_test[1:] > new_y_test[:-1]).astype(int)
@@ -345,8 +329,7 @@ plt.show()
 
 
 
-#pull out the data
-# Assuming new_y_test and test_predictions are the final outputs
+# Pull out the data, assuming new_y_test and test_predictions are the final outputs.
 lstm_results = pd.DataFrame({'Actual': new_y_test, 'Predicted': test_predictions})
 lstm_results.to_csv('lstm_predictions.csv', index=False)
 
